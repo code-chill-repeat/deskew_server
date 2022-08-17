7c55229d5a4a3fab35156da195081e4a70154718
shadyskies <mehta.naman328@gmail.com>
2022-08-17 20:33:34 +0530

    run ocrmypdf as subprocess

--- services.py
+++ services.py
@@ -1,6 +1,8 @@
 import time
 import requests
-import ocrmypdf
+# import ocrmypdf
+import subprocess
+from datetime import datetime
 
 
 # deskew logic and send the status to the backend api server
@@ -11,8 +13,11 @@ def deskew_service(file_path: str):
         # print(10/0)
         # start the deskew service
         start = time.time()
-        ocrmypdf.ocr("1.pdf", "op.pdf", deskew=True, progress_bar=True)
+        print(datetime.now())
+        subprocess.run(["ocrmypdf", "--tesseract-timeout", "300", "1.pdf", "op.pdf", "--deskew"], shell=True, check=True)
+        # ocrmypdf.ocr("1.pdf", "op.pdf", deskew=True, progress_bar=True)
         end = time.time()
+        print(datetime.now())
         print(f"time taken: ", end - start)
         print("deskew completed successfully")
         requests.post("http://localhost:8000/api/deskew_response/",data = {"success":f"Deskewed file: {file_path} successfully"})
